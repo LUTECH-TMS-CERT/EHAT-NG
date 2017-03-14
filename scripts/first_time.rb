0247af7757b5efe26eed855b62bfe9cfb5b4c52d
LUTECH-TMS-CERT <L-TMS-CERT@users.noreply.github.com>
2017-03-14 11:37:58 +0100

    Next Generation Features  (3)
    - Import XML report from Nexpose
    - New Statistics & Charts
    - New risk scoring system w/ auto CVSS Calculator
    - Generate Remediation Plan in XLSX
    - Auto Summarize of findings overview thanks to NLP (Natural Language
    Processing)
    
    ..and much more

--- scripts/first_time.rb
+++ scripts/first_time.rb
@@ -9,9 +9,9 @@ userx = User.first
 if !userx
 	puts "No users in the database, creating a first user. \n"
 
-	puts "Please enter username (default: administrator):  "
+	puts "Please enter username (default: admin):  "
 	username = gets.chomp
-	username = "administrator" if username == ""
+	username = "admin" if username == ""
 
 	puts "Generating random password and adding the Administrator with username #{username}..."
 
@@ -67,9 +67,9 @@ end
 templates = Xslt.first
 
 if !templates
-    puts "Adding the Default Generic Risk Scoring Report Template"
+    puts "Adding the Default Security Report Template"
     xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
-    docx = "./templates/Serpico - GenericRiskScoring.docx"
+    docx = "./templates/Security Report.docx"
 
     xslt = generate_xslt(docx)
     if xslt =~ /Error file DNE/
@@ -83,99 +83,10 @@ if !templates
     datax = Hash.new
     datax["docx_location"] = "#{docx}"
     datax["xslt_location"] = "#{xslt_file}"
-    datax["description"] =  "Generic Risk Scoring Report"
-    datax["report_type"] = "Default Template - Generic Risk Scoring"
+    datax["description"] =  "Security Report"
+    datax["report_type"] = "Default Template"
     report = Xslt.new(datax)
     report.save
-
-	puts "Adding the Default DREAD Report Template"
-	xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
-	docx = "./templates/Serpico - Report.docx"
-
-	xslt = generate_xslt(docx)
-	if xslt =~ /Error file DNE/
-		return "ERROR!!!!!!"
-	end
-
-	# open up a file handle and write the attachment
-	File.open(xslt_file, 'wb') {|f| f.write(xslt) }
-
-	# delete the file data from the attachment
-	datax = Hash.new
-	datax["docx_location"] = "#{docx}"
-	datax["xslt_location"] = "#{xslt_file}"
-	datax["description"] = 	"Default Serpico Report - DREAD Scoring"
-	datax["report_type"] = "Default Template - DREAD Scoring"
-	report = Xslt.new(datax)
-	report.save
-
-	puts "Adding the Default CVSS Report Template"
-	xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
-	docx = "./templates/CVSS_Template.docx"
-
-	xslt = generate_xslt(docx)
-	if xslt =~ /Error file DNE/
-		return "ERROR!!!!!!"
-	end
-
-	# open up a file handle and write the attachment
-	File.open(xslt_file, 'wb') {|f| f.write(xslt) }
-
-	# delete the file data from the attachment
-	datax = Hash.new
-	datax["docx_location"] = "#{docx}"
-	datax["xslt_location"] = "#{xslt_file}"
-	datax["description"] = 	"Default CVSS Report"
-	datax["report_type"] = "Default CVSS Report"
-	report = Xslt.new(datax)
-	report.save
-
-
-	puts "Adding the Serpico Default Finding Template"
-
-	xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
-	docx = "./templates/Serpico - Risk Finding.docx"
-
-	xslt = generate_xslt(docx)
-	if xslt =~ /Error file DNE/
-		return "ERROR!!!!!!"
-	end
-
-	# open up a file handle and write the attachment
-	File.open(xslt_file, 'wb') {|f| f.write(xslt) }
-
-	# delete the file data from the attachment
-	datax = Hash.new
-	datax["docx_location"] = "#{docx}"
-	datax["xslt_location"] = "#{xslt_file}"
-	datax["description"] = 	"Default Serpico Finding"
-	datax["report_type"] = "Default Finding"
-	datax["finding_template"] = true
-	report = Xslt.new(datax)
-	report.save
-
-	puts "Adding the Serpico Default Status Template"
-
-	xslt_file = "./templates/#{rand(36**36).to_s(36)}.xslt"
-	docx = "./templates/Serpico - Finding.docx"
-
-	xslt = generate_xslt(docx)
-	if xslt =~ /Error file DNE/
-		return "ERROR!!!!!!"
-	end
-
-	# open up a file handle and write the attachment
-	File.open(xslt_file, 'wb') {|f| f.write(xslt) }
-
-	# delete the file data from the attachment
-	datax = Hash.new
-	datax["docx_location"] = "#{docx}"
-	datax["xslt_location"] = "#{xslt_file}"
-	datax["description"] = 	"Default Serpico Status"
-	datax["report_type"] = "Default Status"
-	datax["status_template"] = true
-	report = Xslt.new(datax)
-	report.save
 
 else
 	puts "Skipping XSLT creation, templates exist."
