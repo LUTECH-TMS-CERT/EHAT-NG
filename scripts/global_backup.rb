a4efc54c20d1f441c65c451aefb46ad6baeb6519
LUTECH-TMS-CERT <L-TMS-CERT@users.noreply.github.com>
2017-03-16 15:56:26 +0100

    Added Global Backup feature
    see wiki

---
+++ scripts/global_backup.rb
@@ -0,0 +1,48 @@
+require 'zip'
+require 'optparse'
+
+options = {}
+optionparser = OptionParser.new do |opts|
+    opts.banner = "This will backup your current database and screenshots!\n\nUsage: #{$0} [options] \n\n"
+    opts.on("-d", "--directory", "Backup directory") do |d|
+        options[:directory] = d
+    end
+end
+
+begin
+    optionparser.parse!
+rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+    puts "\n\033[33mERROR: #{$!.to_s}\033[0m\n\n"
+    puts optionparser.help
+    exit
+end
+
+if options.empty? || options.length > 2
+    puts optionparser.help
+    exit 1
+end
+
+if options[:directory]
+	bdate  = Time.now()
+
+	db_filename = "./tmp/master-" + (bdate.strftime("%Y%m%d%H%M%S") +".bak")
+	attachments_filename = "./tmp/Attachments" + "-" + (bdate.strftime("%Y%m%d%H%M%S") +".zip")
+	global_filename = "./tmp/EHAT-NG_backup_" + (bdate.strftime("%Y%m%d%H%M%S") +".zip")
+
+	FileUtils::copy_file("./db/master.db", db_filename)
+
+	Zip::File.open(attachments_filename, Zip::File::CREATE) do |zipfile|
+		Dir["./attachments/*" ].each do | name|
+			zipfile.add(name.split("/").last,name)
+		end
+	end
+
+	Zip::File.open(global_filename, Zip::File::CREATE) do |zipfile|
+		zipfile.add(db_filename.split("/").last,db_filename)
+		zipfile.add(attachments_filename.split("/").last,attachments_filename)
+	end
+
+	destination_path = ARGV[0]
+
+	FileUtils::copy_file(global_filename, ARGV[0] + global_filename.split("/").last )
+end
---
+++ scripts/global_restore.rb
@@ -0,0 +1,43 @@
+require 'zip'
+require 'optparse'
+
+Zip.on_exists_proc = true
+
+options = {}
+optionparser = OptionParser.new do |opts|
+    opts.banner = "!!! WARNING !!!\n\n This will destroy your current database!\n\nUsage: #{$0} [options] \n\n"
+    opts.on("-f", "--file", "Restore backup from file") do |f|
+        options[:file] = f
+    end
+end
+
+begin
+    optionparser.parse!
+rescue OptionParser::InvalidOption, OptionParser::MissingArgument
+    puts "\n\033[33mERROR: #{$!.to_s}\033[0m\n\n"
+    puts optionparser.help
+    exit
+end
+
+if options.empty? || options.length > 2
+    puts optionparser.help
+    exit 1
+end
+
+if options[:file] # restore from file
+	Zip::File.open(ARGV[0]) do |zip_file|
+	  # Extract db file
+	  db_backup = zip_file.glob('*.bak').first
+	  db_backup.extract("./db/master.db")
+
+	  attachments_zip = zip_file.glob('*.zip').first
+	  attachments_zip.extract("./tmp/"+attachments_zip.name)
+
+	  Zip::File.open("./tmp/"+attachments_zip.name) do |zip_file2|
+	  # Handle attachments one by one
+		  zip_file2.each do |entry|
+		  	entry.extract("./attachments/"+entry.name)
+		  end
+	  end
+	end
+end
