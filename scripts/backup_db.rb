0063ecc83f07d572803f501cc5bef9a3abf3375e
LUTECH-TMS-CERT <L-TMS-CERT@users.noreply.github.com>
2017-03-16 15:55:52 +0100

    Delete backup_db.rb

--- scripts/backup_db.rb
+++
@@ -1,9 +0,0 @@
-require 'zip'
-
-path = "/tmp/"
-
-bdate  = Time.now()
-filename = path+"master" + "-" + (bdate.strftime("%Y%m%d%H%M%S") +".bak")
-
-FileUtils::copy_file("./db/master.db", filename)
-
